35025ab96b775e0ccf27074cacc8a6a7635c4729
HuyPham <phamminhhuy0901tk@gmail.com>
2025-10-18 23:58:15 +0700

    add ôn luyện

--- app/src/main/java/com/example/apphoctapchotre/MainActivity.java
+++ app/src/main/java/com/example/apphoctapchotre/MainActivity.java
@@ -11,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +21,6 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        tvTest=findViewById(R.id.btnTest);
-        tvTest.setOnClickListener(v -> {
-            Intent intent = new Intent(this, OnLuyen.class);
-            startActivity(intent);
         });
     }
 }
