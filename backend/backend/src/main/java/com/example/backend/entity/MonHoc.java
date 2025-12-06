150347cd6a7b6a04032a39116fb49e4d111c0508
HuyPham <phamminhhuy0901tk@gmail.com>
2025-12-06 20:23:29 +0700

    add monhoc to entity

---
+++ backend/backend/src/main/java/com/example/backend/entity/MonHoc.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author ADMIN
+ */
+@Entity
+@Table(name = "MonHoc")
+public class MonHoc {
+
+    @Id
+    @Column(name = "MaMonHoc", length = 5)
+    private String maMonHoc;
+
+    @Column(name = "TenMonHoc", nullable = false, unique = true, length = 50)
+    private String tenMonHoc;
+
+    public MonHoc(String maMonHoc, String tenMonHoc) {
+        this.maMonHoc = maMonHoc;
+        this.tenMonHoc = tenMonHoc;
+    }
+
+    public MonHoc() {
+    }
+
+    public String getMaMonHoc() {
+        return maMonHoc;
+    }
+
+    public void setMaMonHoc(String maMonHoc) {
+        this.maMonHoc = maMonHoc;
+    }
+
+    public String getTenMonHoc() {
+        return tenMonHoc;
+    }
+
+    public void setTenMonHoc(String tenMonHoc) {
+        this.tenMonHoc = tenMonHoc;
+    }
+    
+}
