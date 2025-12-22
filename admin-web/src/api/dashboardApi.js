357129e71f63fa3162b65175dff7c0b88768f33d
dquocsobug <leduyquoc123meo@gmail.com>
2025-12-22 18:15:45 +0700

    Add call api

--- admin-web/src/api/dashboardApi.js
+++ admin-web/src/api/dashboardApi.js
@@ -4,3 +4,9 @@ export const getDashboard = async () => {
   const res = await axios.get("/api/admin/dashboard");
   return res.data;
 };
+export const getDashboardCharts = async (fromDate, toDate) => {
+  const res = await axios.get("/api/admin/dashboard/charts", {
+    params: { fromDate, toDate }
+  });
+  return res.data;
+};
