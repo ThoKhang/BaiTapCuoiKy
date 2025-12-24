b7900528ea092186643bcdd8113b93f8acd65d62
HuyPham <phamminhhuy0901tk@gmail.com>
2025-12-24 21:23:22 +0700

    .

--- app/src/main/java/com/example/apphoctapchotre/UI/Activity/Video/VideoPlayer.java
+++ app/src/main/java/com/example/apphoctapchotre/UI/Activity/Video/VideoPlayer.java
@@ -1,6 +1,7 @@
 package com.example.apphoctapchotre.UI.Activity.Video;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +9,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.net.Uri;
 import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.example.apphoctapchotre.R;
+import com.example.apphoctapchotre.UI.ViewModel.MediaViewModel;
 
 public class VideoPlayer extends AppCompatActivity {
     private TextView back;
+    private MediaViewModel VideoviewModel;
+    private Long maMedia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +34,7 @@ public class VideoPlayer extends AppCompatActivity {
             startActivity(intent);
         });
         VideoView videoView = findViewById(R.id.videoView);
-
+        maMedia = getIntent().getLongExtra("maMedia", -1);
         String duongDanFile = getIntent().getStringExtra("video_url");
 
         String fullUrl = "http://10.0.2.2:8080/" + duongDanFile;
@@ -41,5 +46,18 @@ public class VideoPlayer extends AppCompatActivity {
         videoView.setMediaController(controller);
 
         videoView.start();
+        SharedPreferences prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        String email=prefs.getString("userEmail",null);
+        videoView.setOnCompletionListener(mp -> {
+            Toast.makeText(this, "Bạn đã xem xong video 🎉", Toast.LENGTH_SHORT).show();
+
+            VideoviewModel.saveProgress(
+                    maMedia,
+                    email,
+                    videoView.getDuration() / 1000,
+                    true
+            );
+        });
+
     }
 }
